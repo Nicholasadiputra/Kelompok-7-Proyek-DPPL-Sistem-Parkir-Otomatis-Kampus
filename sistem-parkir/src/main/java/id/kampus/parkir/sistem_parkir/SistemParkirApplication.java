d44be7653eb5ee9636a2003ed7f4659bcb9e199c
Nicholasadiputra <nicholasa2di2gmail.com>
2025-11-07 18:49:33 +0700

    membuat project spring boot struktur awal

---
+++ sistem-parkir/src/main/java/id/kampus/parkir/sistem_parkir/SistemParkirApplication.java
@@ -0,0 +1,13 @@
+package id.kampus.parkir.sistem_parkir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemParkirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemParkirApplication.class, args);
+	}
+
+}
---
+++ sistem-parkir/src/test/java/id/kampus/parkir/sistem_parkir/SistemParkirApplicationTests.java
@@ -0,0 +1,13 @@
+package id.kampus.parkir.sistem_parkir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemParkirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
